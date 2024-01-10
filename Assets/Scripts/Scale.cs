64174833a47a3b50dbd56e7527871304572398aa
unknown <wnn@yandex.ru>
2024-01-10 23:25:39 +0300

    Fixing the access modifier

--- Assets/Scripts/Scale.cs
+++ Assets/Scripts/Scale.cs
@@ -4,15 +4,15 @@ public class Scale : MonoBehaviour
 {
     [SerializeField] private float _speedScale;
 
-    private Vector3 scaleChange;
+    private Vector3 _scaleChange;
 
     private void Start()
     {
-        scaleChange = new Vector3(_speedScale, _speedScale, _speedScale);        
+        _scaleChange = new Vector3(_speedScale, _speedScale, _speedScale);        
     }
 
     void Update()
     {
-        transform.localScale += scaleChange;        
+        transform.localScale += _scaleChange;        
     }
 }
